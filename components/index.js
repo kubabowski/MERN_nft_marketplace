a3fdf702ac109248ebbacb78bdfbb21ecb7359b9
Kuba <thekubajacob@gmail.com>
2023-12-18 23:40:50 +0100

    main page

--- components/index.js
+++ components/index.js
@@ -1,5 +1,7 @@
 import Navbar from './Navbar';
 import Footer from './Footer';
 import Button from './Button';
+import Banner from './Banner';
+import CreatorCard from './CreatorCard';
 
-export { Footer, Navbar, Button };
+export { Footer, Navbar, Button, Banner, CreatorCard };
